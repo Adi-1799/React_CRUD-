a69753c344f8643a2e56c854a3b79cb5b9fb2d7e
Aditya Narayan Parida <adiprd17@gmail.com>
2022-05-13 16:47:02 +0530

    All set

--- src/App.js
+++ src/App.js
@@ -1,51 +1,97 @@
-import { React,  Component } from 'react';
-
-class App extends Component{
-  constructor(){
-    super();
-    this.state={
-      data : []
-    }
+import React, { useState, useEffect } from 'react'
+import './index.css';
+const getlocalData = ()=> {
+  const  datas = localStorage.getItem("myItems");
+  if(datas){
+    return JSON.parse(datas);
   }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    let data = this.state.data;
-    let write = this.refs.txt.value;
-
-    let newData = {
-      "data" : data
-    }
-    data.push(newData);
-
-    this.setState({
-      data:data
-    })
-
-    this.refs.myForm.reset();
-  }
-
-
-  
-  render(){
-    let data = this.state.data;
-    return(
-      <div>
-
-          <form ref="myForm">
-            <label>Write Something</label>
-            <input type="text" ref="txt"/>
-            <button onClick={e => this.handleSubmit(e)}>Save</button>
-        </form>
-
-        <table>
-          <tr>
-            <th>Entered</th>
-          </tr>
-        </table>
-
-      </div>
-    )
+  else{
+    return [];
   }
 }
-export default App;
+
+export const App = () => {
+  const [inputData , setInputData] = useState("");
+  const [items, setItems] = useState(getlocalData());
+  const [isEditItem, setIsEditItem] = useState("");
+  const [toggleButton, setToggleButton] = useState(false);
+
+  const addItem = () => {
+    if(!inputData){
+      alert("Please enter something");
+    }
+    else if(inputData && toggleButton){
+      setItems(
+        items.map((currElem) => {
+          if(currElem.id === isEditItem){
+            return{
+              ...currElem, name: inputData
+            };
+          }
+          return currElem;
+        })
+      );
+      setInputData([]);
+      setIsEditItem(null);
+      setToggleButton(false);
+    }
+    else{
+      const newInputData = {
+        id:new Date().getTime().toString(), 
+        name:inputData,
+      }; 
+      setItems([...items, newInputData]);
+      setInputData("");
+    }
+  };
+
+  const editItem = (index)=> {
+    const editedItem = items.find((currElem)=> {
+      return currElem.id === index;
+    });
+    setInputData(editedItem.name);
+    setIsEditItem(index);
+    setToggleButton(true);
+  };
+
+  const deleteItem = (index) => {
+    const updatedItems = items.filter((currElem) => {
+      return currElem.id !== index;
+    });
+    setItems(updatedItems);
+  };
+  
+  useEffect(()=>{
+    localStorage.setItem("myItems" ,JSON.stringify(items));
+  }, [items]);
+
+  return (
+    <>
+    <div className='main-child'>
+      <div className='child-div'>
+        <h1>React CRUD </h1>
+        <div className='addItems'>
+          <input type='text' placeholder='Write Something' className='form-control' value={inputData} onChange={(e)=> setInputData(e.target.value)}/>
+          {toggleButton ? (<button className='add-btn' onClick={addItem}>Edit</button>) : (<button className='add-btn' onClick={addItem}>Add</button>)}
+          
+        </div>
+        <div className='showItems'>
+          {items.map((currElem) => {
+            return (
+              <div className='eachItem' key={currElem.id}>
+                <h3>{currElem.name}</h3>
+                <div className='todo-btn'>
+                  <button className='edit-btn' onClick={()=> editItem(currElem.id)}>Edit</button>
+                  <button className='delete-btn' onClick={() => deleteItem(currElem.id)}>Delete</button>
+                </div>                        
+              </div>
+            )
+          })}
+
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+export default App
